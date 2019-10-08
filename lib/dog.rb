e675466d74bc07b8a41d3e44786732908038b164
Yuriy Berezskyy <bereza95@gmail.com>
2019-10-08 19:04:57 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,38 @@
+class Dog
+   def name
+     @name
+   end
+   
+   def breed
+     @breed
+   end
+   
+   def name=(newName)
+     @name = newName
+   end
+   
+   def breed=(newBreed)
+     @breed = newBreed
+   end
+   
+end
+
+class Person 
+  def name 
+    @name 
+  end
+  
+  def job 
+    @job
+  end
+  
+  def name=(newName)
+    @name = newName
+  end
+  
+  def job=(newJob)
+    @job = newJob
+  end
+end
+  def job
+    @
